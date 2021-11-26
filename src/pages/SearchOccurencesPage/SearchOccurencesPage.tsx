5b5e4879cd6c3f35c13ea3351b7435bfa9d9f22a
patrykgawinek <gawinek.patryk@gmail.com>
2021-11-26 16:27:01 +0100

    First fetch for kingdom list

--- src/pages/SearchOccurencesPage/SearchOccurencesPage.tsx
+++ src/pages/SearchOccurencesPage/SearchOccurencesPage.tsx
@@ -1,6 +1,52 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const SearchOccurencesPage = () => {
-  //https://api.gbif.org/v1/species/suggest?rank=kingdom
-  return <main>Occurences search page</main>;
+  const baseUrlApi: string = "https://api.gbif.org/v1";
+  const [kingdomList, setKingdomList] = useState<[number, string][]>([]);
+  const [selectedKingdom, setSelectedKingdom] = useState<number>();
+
+  useEffect(() => {
+    axios
+      .get(`${baseUrlApi}/species/suggest`, {
+        params: {
+          rank: "kingdom",
+        },
+      })
+      .then((response) => {
+        console.log("fetched");
+        let tempList = response.data.map((record: any) => [record.kingdomKey, record.kingdom]);
+        setKingdomList(tempList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [selectedKingdom]);
+
+  return (
+    <main>
+      <h1>Search Occurences</h1>
+      <section>
+        <form action="get">
+          <div>
+            <label htmlFor="kingdom">Kingdom</label>
+            <select
+              name="kingdom"
+              id="kingdom"
+              value={selectedKingdom}
+              onChange={(e) => setSelectedKingdom(parseInt(e.target.value))}
+            >
+              {kingdomList.map((kingdom) => (
+                <option value={kingdom[0]} key={kingdom[0]}>
+                  {kingdom[1]}
+                </option>
+              ))}
+            </select>
+          </div>
+        </form>
+      </section>
+    </main>
+  );
 };
 
 export default SearchOccurencesPage;
