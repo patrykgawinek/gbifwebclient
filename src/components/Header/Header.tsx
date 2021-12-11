62be01518f9da4df376d5012a2c517d127284da0
Patryk Gawinek <patryk.gawinek@student.ap.be>
2021-12-11 02:26:47 +0100

    Added header wrapping to hamburger menu on small devices

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,39 +1,43 @@
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Theme } from "components/App/App";
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
 
 const Header = () => {
   const { darkMode, setDarkMode } = useContext(Theme);
 
   return (
     <header>
-      <Navbar className={darkMode ? "bg-dark navbar-dark" : "bg-primary navbar-dark"}>
+      <Navbar expand="sm" className={darkMode ? "bg-dark navbar-dark" : "bg-primary navbar-dark"}>
         <Container>
           <NavLink to="/">
             <Navbar.Brand>GBIF Client</Navbar.Brand>
           </NavLink>
-          <Nav className="me-auto">
-            <NavLink to="/" exact className="nav-link">
-              How-to
-            </NavLink>
-            <NavLink to="/occurrences" className="nav-link">
-              Search
-            </NavLink>
-            <NavLink to="/map" className="nav-link">
-              Map
-            </NavLink>
-          </Nav>
-          <Button
-            className={darkMode ? "btn-dark" : "btn-primary"}
-            onClick={() => setDarkMode(!darkMode)}
-          >
-            <img
-              width="30px"
-              src={darkMode ? "assets/icons/moon.png" : "assets/icons/sun.png"}
-              alt={darkMode ? "Dark mode" : "Light mode"}
-            />
-          </Button>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink to="/" exact className="nav-link">
+                How-to
+              </NavLink>
+              <NavLink to="/occurrences" className="nav-link">
+                Search
+              </NavLink>
+              <NavLink to="/map" className="nav-link">
+                Map
+              </NavLink>
+            </Nav>
+            <Button
+              className={`${styles.themeToggle} ${darkMode ? "btn-dark" : "btn-primary"}`}
+              onClick={() => setDarkMode(!darkMode)}
+            >
+              <img
+                width="30px"
+                src={darkMode ? "assets/icons/moon.png" : "assets/icons/sun.png"}
+                alt={darkMode ? "Dark mode" : "Light mode"}
+              />
+            </Button>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
